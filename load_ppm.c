14dd86130a8c10a28c99c2d296b160f33e114a7e
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-06-29 23:37:54 +0000

    added headers, comments and sample
    git-svn-id: file:///mnt/data/SVNrepo@1068 a13746d7-5020-4d46-8679-245a07f888a7

--- load_ppm.c
+++ load_ppm.c
@@ -1,10 +1,22 @@
+/*
+	$Date: 2007-06-29 23:37:54 $, $Revision: 1.2 $
+	
+	Simple PPM files loader/identify.
+	
+	Author: Wojciech Mu³a
+	e-mail: wojciech_mula@poczta.onet.pl
+	www:    http://www.mula.w.pl
+	
+	License: public domain
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-char read_int(FILE* f, int* v) {
+char __ppm_read_int(FILE* f, int* v) {
 	int value = 0;	// readed value
-	int c;		// buffer
+	int c;			// buffer
 	char valid = 0;	// flag
 	
 	while (1) {
@@ -24,7 +36,7 @@ char read_int(FILE* f, int* v) {
 	return valid;
 }
 
-char skip_whitespaces(FILE* f) {
+char __ppm_skip_whitespaces(FILE* f) {
 	char skipped = 0;
 	int c;
 	while (1) {
@@ -39,7 +51,7 @@ char skip_whitespaces(FILE* f) {
 	return skipped;
 }
 
-char skip_comment(FILE* f) {
+char __ppm_skip_comment(FILE* f) {
 	int c;
 	c = fgetc(f);
 	if (c != '#') {
@@ -67,6 +79,23 @@ char* PPM_errmsg[] = {
 	/* 8 */ "too few image pixels"
 };
 
+/*
+	Load/identify PPM (i.e. 24bpp RGB).
+
+	Input:
+		f      - file
+
+	Output:
+		width,
+		height - image dimensions
+		maxval - maximal value of sample (usually 255)
+		data   - pointer to image pixels (if NULL image pixels
+		         are not loaded, no memory is allocated)
+				 user have to free data
+
+	Returns:
+		status, 0 if succed, < 0 -- some error occured
+*/
 int load_ppm(FILE* f, int* width, int* height, int* maxval, uint8_t** data) {
 	int c;
 	int size;
@@ -76,16 +105,16 @@ int load_ppm(FILE* f, int* width, int* height, int* maxval, uint8_t** data) {
 		return -1;
 
 	// read width
-	while (skip_whitespaces(f) || skip_comment(f));
-	if (!read_int(f, width)) return -2;
+	while (__ppm_skip_whitespaces(f) || __ppm_skip_comment(f));
+	if (!__ppm_read_int(f, width)) return -2;
 	
 	// read height
-	while (skip_whitespaces(f) || skip_comment(f));
-	if (!read_int(f, height)) return -3;
+	while (__ppm_skip_whitespaces(f) || __ppm_skip_comment(f));
+	if (!__ppm_read_int(f, height)) return -3;
 
 	// read maxval
-	while (skip_whitespaces(f) || skip_comment(f));
-	if (!read_int(f, maxval)) return -4;
+	while (__ppm_skip_whitespaces(f) || __ppm_skip_comment(f));
+	if (!__ppm_read_int(f, maxval)) return -4;
 
 	c = fgetc(f);
 	if (!(c == '\n' || c == '\t' || c == '\r' || c == ' ')) return -5; 
@@ -111,4 +140,37 @@ int load_ppm(FILE* f, int* width, int* height, int* maxval, uint8_t** data) {
 	return 0;
 }
 
-// eof
+#ifdef TEST_PPM
+int main(int argc, char* argv[]) {
+	int i;
+	int width, height, maxval;
+	int result;
+	uint8_t* img;
+	FILE* file;
+	
+	if (argc == 1)
+		puts("usage: program [PPM files]");
+	else
+		for (i=1; i < argc; i++) {
+			file = fopen(argv[i], "rb");
+			if (!file) {
+				fprintf(stderr, "Can't open for read '%s'\n", argv[i]);
+				continue;
+			}
+			
+			result = load_ppm(file, &width, &height, &maxval, &img);
+			if (result < 0)
+				fprintf(stderr, "Error loading file '%s': %s",
+				       argv[i], PPM_errmsg[-result]);
+			else {
+				printf("PPM file '%s' has dimensions %dx%d, image size %d bytes (%sbpp)\n",
+				       argv[i], width, height,
+					   width * height * (maxval < 256 ? 1 : 2),
+					   maxval < 256 ? "8" : "16");
+				free(img);
+			}
+		}
+}
+#endif
+
+// vim: ts=4 sw=4 nowrap noexpandtab
